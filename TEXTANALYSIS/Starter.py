cc2b6dfa6277df60c50b0bb9baa1c2444d54b343
enriqueu99 <98370858+enriqueu99@users.noreply.github.com>
2022-03-29 01:27:39 -0400

    Update Starter.py

--- TEXTANALYSIS/Starter.py
+++ TEXTANALYSIS/Starter.py
@@ -1,12 +1,16 @@
 #Importing praw (REDDIT API)
 #Importing request module to interact with Reddit database
-
+import nltk
 import praw 
 import requests
 import json
 import pandas as pd
 import re
 import csv
+
+
+# variables for :Results for the sentiment analysis and SIA (utilzied)
+
 
 #Client ID and Key needed for API communication
 CLIENT_ID = 'CnNahd6wkmKVY5l5vUAvgQ'
@@ -19,60 +23,66 @@ reddit = praw.Reddit(client_id=CLIENT_ID,client_secret=SECRET_KEY,
 user_agent=user_agent
 
 )
-#blank set is created to store the data
 
 
-
-
-def main():
-    headlines = set()
-    
-    for submission in reddit.subreddit('wallstreetbets').hot(limit=None):
-        headlines.add(submission.title)
-    #use to check if the data is being recorded into the set    
-    print(len(headlines))
-    
-    df = pd.DataFrame(headlines)
-    df.head()
-    df.to_csv('headlines.csv', header=False, encoding='utf-8', index=False)
-
-
-
-
-
-#creating the object for the CSV file where the data is stored
-
-def filter():
-    counter =0
-    keyword = 'Short Opportunity'
-    with open('headlines.csv',encoding='utf-8') as f:
-        for line in csv.reader(f):
-          if keyword in line == True:
-              counter =counter +1
-
-          
-          print(line) 
-           
-           #if keyword in map(str.lower, line):
-              #counter += 1
-    
-    print(counter)
-    return counter
-
-
-
-
+#checks if main file (SAFETY VALVE)
 if __name__ == "__main__":
-    #print(counter)
-    
-    filter()
-   
-    #main()
-    
-    #print(len(infor3))
 
-    #print(infor3)
- 
+    def main():
+        #sentiment analysis module
+        nltk.download('vader_lexicon')
+        from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+        headlines = set()
+        sia = SIA()
+        results = []
+        #iterating through the subreddits user commments (.hot is a time based category)
+        for submission in reddit.subreddit('wallstreetbets').hot(limit=None):
+            headlines.add(submission.title)
+    #use to check if the data is being recorded into the set    
+        #print(len(headlines))** Used to check if data was being stored
+        #create a dataframe for "headlines" using pandas repository
+        df = pd.DataFrame(headlines)
+        df.head()
+        df.to_csv('headlines.csv', header=False, encoding='utf-8', index=False)
+
+
+
+
+
+
+
+        #iterating through the lines of our CSV file 
+        for line in headlines:
+            #utilizing sia to assess the sentiment for given text data 
+            pol_score = sia.polarity_scores(line)
+            pol_score['headline'] = line
+            #appending the pol-scores to 'results'
+            results.append(pol_score)
+        
+        print(results[:])
+        df = pd.DataFrame.from_records(results)
+        df.head()
+        df['label'] = 0
+        #creating thresholds for values in our data-set to determine compounded sentiment analysis
+        df.loc[df['compound'] > .2, 'label'] = 1
+        df.loc[df['compound'] < -.2, 'label'] = -1
+        df.head()
+        df2 = df[['headline','label']]
+        df2.to_csv('reddit_headlines_labels.csv',encoding ='utf-8', index = False)
+        df2.label.value_counts()
+        df.label.value_counts(normalize=True) *100
+        #acquires percentages of value distributions
+        #metric we wanted for aggregate sentiment analysis
+        print(df.label.value_counts(normalize=True) *100)
+        return df.label.value_counts(normalize=True) *100
+    #calling Function
+    
+    main()
+          
+        
+           
+   
+   
 
 
 
